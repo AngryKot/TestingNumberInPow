2e821d85746ee37484cc2858ba0bc5bd32853827
AngryKot <segeda.nikita2016@yandex.ru>
2023-04-13 12:26:27 +0300

    Pow

---
+++ TestingPract13NumberInPow/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the base number: ");
+        int baseNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the power: ");
+        int power = int.Parse(Console.ReadLine());
+
+        int result = (int)Math.Pow(baseNumber, power);
+        int sumOfDigits = GetSumOfDigits(result);
+
+        Console.WriteLine("The result of " + baseNumber + " raised to the power of " + power + " is: " + result);
+        Console.WriteLine("The sum of the digits of the result is: " + sumOfDigits);
+    }
+
+    static int GetSumOfDigits(int number)
+    {
+        int sum = 0;
+
+        while (number != 0)
+        {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
