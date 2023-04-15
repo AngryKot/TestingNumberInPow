89e2e8e2cf3372d02c8502bb9607a579bd1f2126
AngryKot <112689306+AngryKot@users.noreply.github.com>
2023-04-15 22:16:49 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,17 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter the base number: ");
-        int baseNumber = int.Parse(Console.ReadLine());
+        try
+        {
+            Console.Write("Enter the base number: ");
+            int baseNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("Enter the power: ");
-        int power = int.Parse(Console.ReadLine());
+            Console.Write("Enter the power: ");
+            int power = int.Parse(Console.ReadLine());
 
-        int result = (int)Math.Pow(baseNumber, power);
-        int sumOfDigits = GetSumOfDigits(result);
+            int result = (int)Math.Pow(baseNumber, power);
+            int sumOfDigits = GetSumOfDigits(result);
 
-        Console.WriteLine("The result of " + baseNumber + " raised to the power of " + power + " is: " + result);
-        Console.WriteLine("The sum of the digits of the result is: " + sumOfDigits);
+            Console.WriteLine("The result of " + baseNumber + " raised to the power of " + power + " is: " + result);
+            Console.WriteLine("The sum of the digits of the result is: " + sumOfDigits);
+        }
+        catch
+        {
+            Console.WriteLine("Error: Wrong number input");
+        }
     }
 
     static int GetSumOfDigits(int number)
